03d99b2c1605c4ea9ea34c8df3ffd4980e069b04
Levi Gleik <leviacedo1@gmail.com>
2025-12-14 16:43:13 -0300

    refactor: remove brand

--- src/app/[locale]/brand/page.tsx
+++
@@ -1,9 +0,0 @@
-import TableBrand from "@/app/[locale]/brand/components/table/table-brand";
-
-export default async function BrandPage() {
-	return (
-		<div className="flex flex-col p-6">
-			<TableBrand />
-		</div>
-	);
-}
